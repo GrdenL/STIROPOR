1cbd81806756b57a7a1bae88ca51e2ede6e485a8
AndrijaBilic <andrija.bilic@fer.hr>
2026-01-20 20:22:20 +0100

    added offer trade button for admin

--- frontend/src/pages/GameDetailsPage.jsx
+++ frontend/src/pages/GameDetailsPage.jsx
@@ -281,7 +281,7 @@ const GameDetailsPage = () => {
                               : "Show image"}
                           </button>
 
-                          {!isOwnListing && !isAdmin && (
+                          {!isOwnListing && (
                             <Link
                               to={`/offer/${listing.listingId}`}
                               className="text-sm bg-vintage-accent text-white px-4 py-2 rounded-full hover:bg-amber-700"
